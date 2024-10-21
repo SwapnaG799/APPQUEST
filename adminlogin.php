82cf02401341941eb66bffd613db2f93e5ed65fd
Gadde Veeraswapna <swapnagadde77@gmail.com>
2024-10-21 19:45:07 +0530

    Add files via upload

---
+++ adminlogin.php
@@ -0,0 +1,72 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "DonationData";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Get user input from form
+$input_username = $_GET['username'];
+$input_password = $_GET['password'];
+
+if($input_username=='vasu@gmail.com' and $input_password=='1234')
+{
+    $sql = "SELECT * FROM DonarDetails";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo '<h2 style="text-align:center;color:auqa;font-variant:small-caps;">Donar Details</h2>';
+        echo '<table  border="1px" border-collapse: separate  style="text-align: center;  margin-left: auto; margin-right: auto;">';
+        echo '<tr><th>Donarname</th><th>mobilenumber </th><th>Donar_location </th><th>foodtype</th><th>quantity</th></tr>';
+
+        // Output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td style='color:red;'>" . $row["Donarname"] . "</td>";
+            echo "<td>" . $row["mobilenumber"] . "</td>";
+            echo "<td>" . $row["Donar_location"] . "</td>";
+            echo "<td>" . $row["foodtype"] . "</td>";
+            echo "<td>" . $row["quantity"] . "</td>";
+            
+            echo "</tr>";
+        }
+        echo '</table>';
+    } else {
+        echo "No records found in the DonarDetails table.";
+    }
+    // volunteer details
+
+    $sql = "SELECT * FROM VolunteerDetails";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo '<h2 style="text-align:center;color:auqa;font-variant:small-caps;">Volunteer Details</h2>';
+        echo '<table  border="1px" border-collapse: separate  style="text-align: center;  margin-left: auto; margin-right: auto;">';
+        echo '<tr><th>Volunteer Name</th><th>Volunteer Email </th></tr>';
+
+        // Output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td style='color:red;'>" . $row["volunteername"] . "</td>";
+            echo "<td>" . $row["email"] . "</td>";
+            
+            echo "</tr>";
+        }
+        echo '</table>';
+    } else {
+        echo "No records found in the DonarDetails table.";
+    }
+
+
+
+}
+else{
+    echo '<script type="text/javascript">
+    alert("Entered wrong details.");
+    window.location.href = "adminlogin.html";
+  </script>';
+}
